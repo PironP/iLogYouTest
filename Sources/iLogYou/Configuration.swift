37696d0e688ba22d02e180d2f1ab690dcd5cfae8
pironp <piron.pierre@gmail.com>
2019-02-07 12:43:35 +0100

    initial commit

---
+++ Sources/iLogYou/Configuration.swift
@@ -0,0 +1,17 @@
+//
+//  Configuration.swift
+//  CodeWriter
+//
+//  Created by Léo LEGRON on 15/01/2019.
+//
+
+import Foundation
+
+class Configuration {
+    
+    public var projectName = ""
+    public var userClassFields = [String:String]()
+    public var APIAddress = ""
+    public var APIRoutes = [String: String]()
+    
+}
---
+++ Sources/iLogYou/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  iLogYou
+//
+//  Created by Léo LEGRON on 15/01/2019.
+//  Copyright © 2019 Léo LEGRON. All rights reserved.
+//
+
+import Foundation
+import XcodeGenKit
+
+var cli = CLI()
+
+cli.welcome()
+cli.askForProjectName()
+//cli.askForUserModelFields()
+//cli.askForAPIAddress()
+//cli.askForAPIRoutes()
+
+var fileGenerator = FileGenerator(configuration: cli.configuration)
+
+fileGenerator.generateDirectories()
+fileGenerator.generateUserModel()
+fileGenerator.generateBasicFiles()
+fileGenerator.generateViewControllers()
+fileGenerator.generateXcodeProj()
+
+cli.end()
+
+//
+// 1. Project Name
+// 2. User Model
+// 3. API address
+// 4. API routes :
+//    - login
+//    - signup
+//    - forgotten password
+//    - delete user
+//
+
+//
+// File architecture of iLogYou Framework
+//
+// - iLogYou directory
+// ---- AppDelegate.swift
+// ---- LoginViewController.swift (conditionnal)
+// ---- SignUpViewController.swift (conditionnal)
+// ---- ProfileViewController.swift (conditionnal)
+// ---- DeleteUserViewController.swift (conditionnal)
+// ---- PasswordForgottenViewController.swift (conditionnal)
+// ---- Assets.xcassets directory
+// -------- AppIcon.appiconset
+// ------------ AContents.json
+// -------- Contents.json
+// ---- Base.lproj directory
+// -------- LaunchScreen.storyboard
+// ---- data directory
+// -------- dto directory
+// ------------ Services.swift
+// ------------ APIProvider.swift
+// --------- model directory
+// ------------ (Name of user class).swift
+// ---- info.plist
+//
